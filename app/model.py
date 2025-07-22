269051d936ba78db4363c01ccbd3db8fdf75de7d
amol1111111 <kadamamolshivaji1111@gmail.com>
2025-07-22 19:51:47 +0530

    model.py

--- app/model.py
+++ app/model.py
@@ -7,7 +7,16 @@ def train_and_save_model():
     X = df.drop("Outcome", axis=1)
     y = df["Outcome"]
 
-    model = RandomForestClassifier()
+    model = RandomForestClassifier(
+    n_estimators=100,        # Number of trees in the forest
+    max_depth=10,            # Maximum depth of the tree
+    min_samples_split=5,     # Minimum number of samples required to split an internal node
+    min_samples_leaf=2,      # Minimum number of samples required to be at a leaf node
+    max_features='sqrt',     # Number of features to consider when looking for the best split
+    bootstrap=True,          # Whether bootstrap samples are used when building trees
+    random_state=42          # Seed for reproducibility
+    )
+
     model.fit(X, y)
 
     with open("model/diabetes_model.pkl", "wb") as f:
